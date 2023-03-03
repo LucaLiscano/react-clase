12fbaa35e034902a55f7b6c2958a51cd0f832560
unknown <luca.liscano@anima.edu.uy>
2023-03-03 12:33:55 -0300

    styles

--- src/index.js
+++ src/index.js
@@ -2,31 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Greating, UserCard} from './Greating'
 import {Button} from './Button'
+import { TaskCard } from './task'
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <Button text='click me'/>
-  <Button text='pay'/>
-  <Button text='' name="joe"/>
-
-    {/* <UserCard
-      name="Ryan ray"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 main street", city: "Ney York" }}
-      greet={function () {
-        alert("hello");
-      }}
-    />
-    <UserCard
-      name="joe Mcmillan"
-      amount={1000}
-      married={false}
-      points={[100,20]}
-      address={{ street: "av some 123", city: "Ney York" }}
-    /> */}
+  <TaskCard  ready={true}/>
   </>
 );
 
